d2e5b69ff2fcec0ab4dbc146d7f954608595de2f
John El Basha <john.elbasha@edgepetrol.com>
2023-02-08 05:51:26 +0400

    Moved static data into database using json-server package.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
+import { useEffect, useState } from "react";
 import AddItem from "./components/AddItem";
 import Header from "./components/Header";
 import Items from "./components/Items";
 
 function App() {
-  const items = [
-    {
-      id: 1,
-      name: 'Jam'
-    },
-    {
-      id: 2,
-      name: 'bread'
-    },   {
-      id: 3,
-      name: 'milk'
-    },   {
-      id: 4,
-      name: 'eggs'
-    },   {
-      id: 5,
-      name: 'coffee'
-    },
-  ];
+  const [items, setItems] = useState([])
   
+  useEffect(() => {
+    const getItems = async () => {
+      const itemsFromServer = await fetchItems();
+      setItems(itemsFromServer);
+    }
+
+    getItems();
+  }, [])
+
+  const fetchItems = async () => {
+    const res = await fetch('http://localhost:5002/items');
+    const data = await res.json()
+
+    return data
+  }
+
   return (
     <div className="container">
       <Header />
