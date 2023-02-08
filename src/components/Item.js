a3c05082f7d1164c7120a7d56c41a5b37df2a31a
John El Basha <john.elbasha@edgepetrol.com>
2023-02-08 04:41:43 +0400

    Installed react-icons package and added delete icon to Item component.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,7 +1,9 @@
+import { FaTrashAlt } from 'react-icons/fa'
+
 const Item = ({ item }) => {
   return (
     <div>
-      <h3>{item.name}</h3>
+      <h3>{item.name} <FaTrashAlt /></h3>
     </div>
   )
 }
